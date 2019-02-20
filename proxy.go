25d6ae76aa35970601051a181420d4739bcde5ec
Jarota <32527884+Jarota@users.noreply.github.com>
2019-02-20 21:41:13 +0000

    trying to get https up and running

--- proxy.go
+++ proxy.go
@@ -2,22 +2,92 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"net"
 	"net/http"
+	"regexp"
 )
 
-func main() {
+var hasPort = regexp.MustCompile(`:\d+$`)
+
+func removeProxyHeaders(r *http.Request) {
+	r.RequestURI = ""
+	r.Header.Del("Proxy-Connection")
+	r.Header.Del("Proxy-Authenticate")
+	r.Header.Del("Proxy-Authorization")
+	// Connection, Authenticate and Authorization are single hop Header:
+	// http://www.w3.org/Protocols/rfc2616/rfc2616.txt
+	// 14.10 Connection
+	//   The Connection general-header field allows the sender to specify
+	//   options that are desired for that particular connection and MUST NOT
+	//   be communicated by proxies over further connections.
+	r.Header.Del("Connection")
+}
+
+func copyAndClose(dst, src *net.TCPConn) {
+	if _, err := io.Copy(dst, src); err != nil {
+		fmt.Printf("Error copying to client: %s", err)
+	}
+	dst.CloseWrite()
+	src.CloseRead()
+}
+
+func handleHTTP(w http.ResponseWriter, req *http.Request) {
 	client := &http.Client{}
-	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Contacting", r.URL.Hostname())
-		r.RequestURI = ""
-		resp, err := client.Do(r)
-		if err != nil {
-			log.Fatal(err)
-		}
-		resp.Write(w)
-		//w.Write([]byte("RECEIVED RESPONSE"))
-	}))
-	fmt.Println("Listening...")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Printf("Rquest received: %s %s\n", req.Method, req.URL)
+	removeProxyHeaders(req)
+
+	if req.Method == "CONNECT" {
+		handleHTTPS(w, req)
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Response received: %s\n\n", res.Status)
+	res.Write(w)
+
+	req.Body.Close()
+	res.Body.Close()
+}
+
+func handleHTTPS(w http.ResponseWriter, req *http.Request) {
+
+	hij, ok := w.(http.Hijacker)
+	if !ok {
+		panic("httpserver does not support hijacking")
+	}
+
+	proxyClient, _, err := hij.Hijack()
+	if err != nil {
+		panic("Cannot hijack connection " + err.Error())
+	}
+
+	host := req.URL.Host
+	if !hasPort.MatchString(host) {
+		host += ":80"
+	}
+
+	targetSiteCon, err := net.Dial("tcp", host)
+	if err != nil {
+		log.Fatal("OOPSIE " + err.Error())
+	}
+
+	fmt.Printf("Accepting CONNECT to %s\n", host)
+	proxyClient.Write([]byte("HTTP/1.0 200 OK\r\n\r\n"))
+	targetTCP, targetOK := targetSiteCon.(*net.TCPConn)
+	proxyClientTCP, clientOK := proxyClient.(*net.TCPConn)
+	if targetOK && clientOK {
+		go copyAndClose(targetTCP, proxyClientTCP)
+		go copyAndClose(proxyClientTCP, targetTCP)
+	}
+}
+
+func main() {
+	httpHandler := http.HandlerFunc(handleHTTP)
+	fmt.Printf("Proxy activated!\n\n")
+	http.ListenAndServe(":8080", httpHandler)
 }
